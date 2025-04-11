5d943487b001cbfddb71c48669899aa1f0a5e7aa
ismail <ismailonline@gmail.com>
2025-04-11 17:44:34 +0300

    Logout butonu eklendi

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import withAuth from "../../lib/withAuth";
+import LogoutButton from "../../components/LogoutButton";
 
 function ProfilePage() {
   return (
     <div className="p-10 text-center">
       <h1 className="text-3xl font-bold">Profil Sayfası 👤</h1>
       <p className="mt-4 text-gray-600">Sadece giriş yapan kullanıcılar bu sayfayı görebilir.</p>
+
+      <LogoutButton />
     </div>
   );
 }
